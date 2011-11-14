9859cb829c24694a290143b3319bcd3807e6eb0b
Joel Dietz <jdietz+github@gmail.com>
2011-11-14 16:02:08 +0100

    updated submodules

--- Dragon-Breath-Export.sh
+++ Dragon-Breath-Export.sh
@@ -1,4 +1,14 @@
 # Export script for all Dragon-Breath components to Eclipse workspaces, usually run after updating to current version (and tests!)
 
-cp -R ~/tech/Dragon-Breath/src/classes/*  ~/tech/eclipse/Pipeline2/src/classes
+MYPATH="~/tech/eclipse/Pipeline/src/classes"
+echo "Exporting to"
+echo $MYPATH
 
+cp -R ~/tech/Dragon-Breath/src/classes/*  ~/tech/eclipse/Pipeline/src/classes
+
+# Do we need anything besides classes?
+#cp -R ~/tech/Dragon-Breath/src/classes/*  ~/tech/eclipse/Pipeline/src/classes
+
+# add namespace
+
+# add missing xml files
--- Dragon-Breath-Update.sh
+++ Dragon-Breath-Update.sh
@@ -1,8 +1,10 @@
 # Update script for all Dragon-Breath components, stored locally as separate git repositories
 
-git submodule update
 echo "Updating all submodules"
-PROJECTS="SmartFactory ApexCommonsCore"
+git submodule update
+PROJECTS="SmartFactory ApexLimitUtil ApexTestUtil ApexCommonsString ApexCommonsHttpUtil"
 echo $PROJECTS
 
-cp -R ./SmartFactory-for-Force.com/src/classes/* ~/tech/Dragon-Breath/src/classes
+cp -R ./submodules/SmartFactory/src/classes/* ~/tech/Dragon-Breath/src/classes
+cp -R ./submodules/ApexLimitUtil/* ~/tech/Dragon-Breath/src/classes
+cp -R ./submodules/ApexTestUtil/* ~/tech/Dragon-Breath/src/classes
