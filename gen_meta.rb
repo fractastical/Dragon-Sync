7928496834bfe3c94b1500e81f002e0ca98c1125
Joel Dietz <jdietz+github@gmail.com>
2011-11-24 11:48:27 +0100

    Slowly rubyifying bash commands

--- gen_meta.rb
+++ gen_meta.rb
@@ -1,63 +1,83 @@
 class Gen_Meta 
      
-     api_version = 22.0
-                     
-
-
-    meta_template = File.open("/Users/jdietz/tech/Dragon-Breath/META", "r")
-    meta_template_contents = meta_template.sysread(meta_template.stat.size)
-
-    puts "Iterating through all "  + Dir["src/classes/*.cls"].size.to_s + " classes"
-    counter = 0      
-    
-    hash = { :class => { :extension => "cls", :foldername => "classes", :metaxml =>
-      <<-EOS
+    api_version = 22.0 
+    page = ""
+                         
+    sfdc_package_components = { 
+      
+        :class => { :displayname => "Class", :pluralname => "Classes", :extension => "cls", :foldername => "classes", :metaxml =>
+           
+           <<-EOS
               <?xml version="1.0" encoding="UTF-8"?>
               <ApexClass xmlns="http://soap.sforce.com/2006/04/metadata">
                 <apiVersion>#{api_version}</apiVersion>
               </ApexClass>
             EOS
+              
               },
-             :staticresources => { :extension => "resource", :foldername => "staticresources", :metaxml =>
+         
+         :staticresource => { :displayname => "Static Resource", :pluralname => "Static Resources", :extension => "resource", :foldername => "staticresources", :metaxml =>
                 <<-EOS
                         <?xml version="1.0" encoding="UTF-8"?>
                         <ApexClass xmlns="http://soap.sforce.com/2006/04/metadata">
                           <apiVersion>#{api_version}</apiVersion>
                         </ApexClass>
                       EOS
-           }  }
+          
+           },
+          
+          :visualforcepage => { :displayname => "Visualforce Page", :pluralname => "Visualforce pages", :extension => "page", :foldername => "pages", :metaxml =>
+               <<-EOS
+                 <?xml version="1.0" encoding="UTF-8"?>
+                 <ApexClass xmlns="http://soap.sforce.com/2006/04/metadata">
+                   <apiVersion>#{api_version}</apiVersion>
+                   <label>#{page}</label>
+                 </ApexClass>
+                EOS
+
+           },
+                             
+           :trigger => { :displayname => "Apex Trigger", :pluralname => "Apex Triggers", :extension => "trigger", :foldername => "triggers", :metaxml =>
+             <<-EOS
+               <?xml version="1.0" encoding="UTF-8"?>
+               <ApexTrigger xmlns="http://soap.sforce.com/2006/04/metadata">
+                   <apiVersion>#{api_version}</apiVersion>
+                   <status>Active</status>
+               </ApexTrigger>
+             EOS
+                
+           },
+
+           :component => { :displayname => "Visualforce Component", :pluralname => "Visualforce Components", :extension => "component", :foldername => "components", :metaxml =>
+               <<-EOS
+               <?xml version="1.0" encoding="UTF-8"?>
+               <ApexComponent xmlns="http://soap.sforce.com/2006/04/metadata">
+                   <apiVersion>#{api_version}</apiVersion>
+                   <label>Recruit Listing</label>
+               </ApexComponent>
+               EOS
+           }
+    }
     
-    hash.each do |k,v|      
-      Dir["src/#{hash[:k][:foldername]}/*.#{hash[:k][:extension]}"].each do |file|
+    sfdc_package_components.each do |k,v|   
+         
+      puts "Iterating through all "  + Dir["src/#{v[:foldername]}/*.#{v[:extension]}"].size.to_s + " " + v[:pluralname].downcase
+      counter = 0      
 
-    end
-    
-    Dir["src/classes/*.cls"].each do |file|
-      meta = "#{file}-meta.xml"
-      next if file =~ /-meta\.xml$/ || File.exist?(meta)
-          counter += 1
-          puts meta + " does not exist, attempting to create"
-          File.open(meta, 'w') do |f|            
-            f.puts(class_xml)
-          end
-    end
+      Dir["src/#{v[:foldername]}/*.#{v[:extension]}"].each do |file|
+        meta = "#{file}-meta.xml" 
+        page = file
+        next if file =~ /-meta\.xml$/ || File.exist?(meta)
+            counter += 1
+            puts meta + " does not exist, attempting to create"
+            File.open(meta, 'w') do |f|            
+              f.puts(v[:metaxml])
+            end
+    end  
 
-    puts "No classes found without metadata." if counter==0    
+    puts "No " + v[:pluralname] + " found without metadata." if counter==0    
 
-    puts "Iterating through all "  + Dir["src/resources/*.resources"].size.to_s + " resources"
-    counter = 0      
-    
-    Dir["src/staticresources/*.resource"].each do |file|
-      meta = "#{file}-meta.xml"
-      next if file =~ /-meta\.xml$/ || File.exist?(meta)
-          counter += 1
-          puts meta + " does not exist, attempting to create"
-          File.open(meta, 'w') do |f|            
-            f.puts(resource_xml)
-          end
-    end
-
-    puts "No resources found without metadata." if counter==0    
+  end
     
     
 end
